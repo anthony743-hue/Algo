f344ba20929808be9f09342f7a15daa588556ac7
Faniry <faniryanthony94@gmail.com>
2026-04-13 06:11:33 +0300

    "wip2"

---
+++ symptoms_202604121108.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.symptoms ("name",description,severity_scale,created_at) VALUES
+	 ('Headache','Pain or discomfort in the head or face area.',3,'2026-03-15 14:20:59.311282+03'),
+	 ('Fever','Temporary increase in body temperature, often due to an illness.',5,'2026-03-15 14:20:59.311282+03'),
+	 ('Cough','A sudden, forceful hacking sound to release air and clear an irritation in the throat.',4,'2026-03-15 14:20:59.311282+03'),
+	 ('Fatigue','A lingering tiredness that is constant and limiting.',4,'2026-03-15 14:20:59.311282+03'),
+	 ('Nausea','A sensation of unease and discomfort in the upper stomach with an urge to vomit.',5,'2026-03-15 14:20:59.311282+03'),
+	 ('Shortness of Breath','Difficulty breathing or feeling like you cannot get enough air.',9,'2026-03-15 14:20:59.311282+03'),
+	 ('Chest Pain','Discomfort or pain in the chest area, potentially indicating a heart issue.',10,'2026-03-15 14:20:59.311282+03'),
+	 ('Dizziness','A sensation of spinning or feeling lightheaded and unsteady.',6,'2026-03-15 14:20:59.311282+03'),
+	 ('Sore Throat','Pain, scratchiness or irritation of the throat that often worsens when swallowing.',2,'2026-03-15 14:20:59.311282+03'),
+	 ('Muscle Pain','Aching or discomfort in the muscles, often associated with viral infections.',3,'2026-03-15 14:20:59.311282+03');
